afca167c70392f65a3d60c90af2aa25b2eb42923
hamza-b-m <hamza.ben.manaa@gmail.com>
2020-09-23 11:25:47 +0100

    React Props checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Profile from "./profile/Profile";
+import myimage from "./res/imgcv.jpg";
+import "./App.css";
 
 function App() {
+  let Name = "Hamza Ben Manâa";
+  let bio = " I'm a full stack developer";
+  let prof = "developer";
+
+  const show = (e) => alert(e);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile bio={bio} prof={prof} show={show}>
+        <img src={myimage} />
+      </Profile>
     </div>
   );
 }
---
+++ src/profile/Profile.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = (props) => {
+  return (
+    <>
+      <div>{props.Name}</div>
+      <div>{props.bio}</div>
+      <div>{props.prof}</div>
+      <button onClick={() => props.show(props.Name)}>Click me</button>
+    </>
+  );
+};
+
+Profile.defaultProps = {
+  Name: "Ahmed Ben MAnâa",
+  bio: "i enjoy traveling",
+  prof: "engineer",
+};
+
+Profile.propTypes = {
+  Name: PropTypes.string,
+  bio: PropTypes.string,
+  prof: PropTypes.string,
+  alert: PropTypes.func,
+};
+
+export default Profile;
